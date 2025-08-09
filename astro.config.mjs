06620ad24a31844a6c4a707d4af13a96ebd1b9e6
David Vivanco <deivid42@hotmail.es>
2025-08-09 12:32:00 -0500

    corregir ruta del menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
 				{
 					label: 'Guias',
 					items: [
-						{ label: 'SDKMAN', slug: 'guides/example' },
+						{ label: 'SDKMAN', slug: 'guias/sdkman' },
 					],
 				}
 			],
